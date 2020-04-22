98439484310955f2b3f49693afd011d162dd96fc
Alaa Adel <alaaadel@Alaas-Mac.local>
2020-04-22 21:01:30 +0200

    Initial Commit

---
+++ World News/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  World News
+//
+//  Created by Alaa Adel on 4/22/20.
+//  Copyright © 2020 Alaa Adel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
